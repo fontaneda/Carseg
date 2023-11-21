16dcb6d736ac30353e58aca7eefa3fe112583cb1
Felix Ontaneda <151570600+fontaneda@users.noreply.github.com>
2023-11-21 09:05:51 -0500

    Add files via upload

---
+++ WebCallCenter - GitHub/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("WebCallCenterAutomatizado")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebCallCenterAutomatizado")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("b8dce5d1-1e79-4b4d-8426-1db67ccf54ca")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
